5d1a12a59577efe9c0626200f2b3a707fcb57ae3
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-06-08 17:07:15 -0500

    added glassmorphism logo to navbar, adjusting navbar parameters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import '../styles/Navbar.css';
+import Logo from '../assets/glass-logo.png';
 
 function Navbar() {
     return (
@@ -6,14 +7,15 @@ function Navbar() {
 
             <header className='header'>
 
-                <a href='https://www.google.com/' className='logo'>Logo</a>
+            <a href='https://www.google.com/' className='logo'>
+                <img src={Logo} id='glass-icon' />
+            </a>
 
                 <navbar className='navbar'>
                     <a href='https://www.google.com/'>Home</a>
-                    <a href='https://www.google.com/'>About</a>
-                    <a href='https://www.google.com/'>Portfolio</a>
-                    <a href='https://www.google.com/'>Services</a>
-                    <a href='https://www.google.com/'>Contact</a>
+                    <a href='https://www.google.com/'>Login</a>
+                    <a href='https://www.google.com/'>Sign Up</a>
+                    
                 </navbar>
 
             </header>
