4c5fd7560ea94a9fe6c379ee0c797a64791bfc43
Rick3Mrtz <Rick.mrtz3@gmail.com>
2023-06-09 05:19:18 -0500

    added Signup.js with matching effects, subtle changes to input styles

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import '../styles/Signup.css'
 import Login from './Login';
+import Background from './Background';
 
 function Signup() {
     return (
         <div>
-            <Login />
+        <div className='signup-screen'>
+            
+            
+        <div class="signup-bg">
+          <form>
+            <h1>Sign Up Here</h1>
+
+            <label for="name">Full Name</label>
+            <input type="text" placeholder="Name"/>
+
+            <label for="password">Email</label>
+            <input type="password" placeholder="Email Address"/>
+
+            <label for="username">Username</label>
+            <input type="text" placeholder="Username or Phone"/>
+
+            <label for="password">Password</label>
+            <input type="password" placeholder="Password"/>
+
+            <button type="submit">Log In</button>
+          </form>
         </div>
+        
+      </div>
+      <Background />
+      </div>
     )
 }
 
